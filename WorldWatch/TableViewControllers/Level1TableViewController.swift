50045f0cbf0ff7d6972d14cb1c291c2440b3d562
Emery Clark <emery@kw4bq.net>
2020-12-05 19:16:53 -0500

    Update Level1TableViewController.swift

--- WorldWatch/TableViewControllers/Level1TableViewController.swift
+++ WorldWatch/TableViewControllers/Level1TableViewController.swift
@@ -14,16 +14,13 @@ class Level1TableViewController: UITableViewController, UINavigationControllerDe
 
     //MARK: Properties
     
-    var result: [String: [String: [String]]] = [:]
-    //var big = [String]()
+    //var result: [String: [String: [String]]] = [:]
     let root = Node("")
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
         self.clearsSelectionOnViewWillAppear = false
-                
 
         loadTimeZoneData()
     }
@@ -32,10 +29,9 @@ class Level1TableViewController: UITableViewController, UINavigationControllerDe
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         
-        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
-        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        let isPresenting = presentingViewController is UINavigationController
         
-        if isPresentingInAddMealMode {
+        if isPresenting {
             dismiss(animated: true, completion: nil)
         }
         else if let owningNavigationController = navigationController{
@@ -59,16 +55,13 @@ class Level1TableViewController: UITableViewController, UINavigationControllerDe
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Table view cells are reused and should be dequeued using a cell identifier.
         
         let cellIdentifier = "Level1TableViewCell"
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? Level1TableViewCell  else {
             fatalError("The dequeued cell is not an instance of Level1TableViewCell.")
         }
-        
-        // Fetches the appropriate meal for the data source layout.
-        
+                
         let region = root.children[indexPath.row]
         
         cell.timezoneTextLabel.text = region.value
@@ -95,28 +88,11 @@ class Level1TableViewController: UITableViewController, UINavigationControllerDe
                 }
             }
         }
-        
-//        result = TimeZone.knownTimeZoneIdentifiers.reduce(into: [:]) {
-//            if let index = $1.firstIndex(of: "/") {
-//                let key = String($1[..<index])
-//                let value = String($1[$1.index(after: index)...])
-//                if let index = value.firstIndex(of: "/") {
-//                    let country = String(value[..<index])
-//                    let city = String(value[value.index(after: index)...])
-//                    $0[key, default: [:]][country, default: []].append(city)
-//                } else {
-//                    $0[key, default: [:]][value] = []
-//                }
-//            }
-//        }
-//
-//        big.append(contentsOf: result.keys.sorted { $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending })
     }
     
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         super.prepare(for: segue, sender: sender)
@@ -137,10 +113,7 @@ class Level1TableViewController: UITableViewController, UINavigationControllerDe
                 fatalError("The selected cell is not being displayed by the table")
             }
             
-            //let selectedBig = root.children[indexPath.row]
-            //print("Selected big ", selectedBig)
             level2TableViewController.selectedL1IndexPath = indexPath
-            //level2TableViewController.selectedBig = selectedBig.value
             level2TableViewController.root = root
             
         default:
